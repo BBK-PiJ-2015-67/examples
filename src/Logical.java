dcb8e0cd4af3e1c116ce36613ebdf3ff4db24da3
Laurent Mignot <laurent@mignot.eu>
2016-04-26 18:44:28 +0100

    fooling around with logical operators

---
+++ src/Logical.java
@@ -0,0 +1,11 @@
+/**
+ * @author lmignot
+ */
+public class Logical {
+
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+
+        System.out.println("Testing and: " + (a != 1 && a != 2));
+    }
+}
